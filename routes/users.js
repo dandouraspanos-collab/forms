ed8e3ed4f3df1209644d33d1a881768ef194836f
Panos Dandouras <dandouraspanos@gmail.com>
2026-03-25 20:13:25 -0400

    final homework stages

--- routes/users.js
+++ routes/users.js
@@ -6,37 +6,52 @@ router.route('/').get((req, res) => {
     res.send('User List');
 }).post((req, res) => {
     const firstName = req.body.firstName;
-    const isValid = firstName !=="";
+    const lastName = req.body.lastName;
+    const gender = req.body.gender;
+    const age = req.body.age;
+
+    const isValid = 
+    firstName !=="" && 
+    lastName !=="" && 
+    gender !=="" && 
+    age !=="";
+
         if(isValid){
-            console.log(`Adding user: ${firstName}`);
-            users.push({firstName});
+            console.log(`Adding user: ${firstName} ${lastName}`);
+            users.push({firstName, lastName, gender, age});
             res.render('users/list', {users});
         }
         else{
             console.log('Error adding user!');
-            res.render('users/new', {firstName:firstName});
+            res.render('users/new', {firstName:firstName, lastName:lastName, gender:gender, age:age});
         }     
 });
-router.get('list', (req, res) => {
+router.get('/list', (req, res) => {
     res.render('users/list', {users});
 });
 router.get('/new', (req, res) => {
-    res.render('users/new', {firstName:'Test'})
+    res.render('users/new', {firstName:''})
 });
 //  router.get('/:id', (req, res) => {
     //     res.send(`Getting User Data for ID: ${req.params.id}`);
     // });
 router.route('/:id').get((req, res)=>{
     console.log(req.user);
-    console.log('Getting user data!')
-    res.send(`Getting User data for id: ${req.user['name']}`);
+    console.log('Getting user data!');
+
+    if (!req.user) {
+        return res.send('User not found');
+    }
+
+    res.render('users/id', {user: req.user, id: req.params.id});
 }).delete((req, res)=>{
     res.send(`Deleting User data for id: ${req.params.id}`);
 }).put((req, res)=>{
     res.send(`Updating User data for id: ${req.params.id}`);
 });
 
-const users = [{firstName:"Panos"}, {firstName:"Stavros"}];
+const users = [{firstName:"Panos", lastName:"Dandouras", gender:"Male", age:20}, 
+    {firstName:"Stavros", lastName:"Dandouras", gender:"Male", age:18}];
 router.param("id", (req, res, next, id) => {
     req.user = users[id];
     next();
